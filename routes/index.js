4123e7a79b27761b7b1021d44d80a6714f26c2b1
daguila27 <daguila.gallardo@gmail.com>
2017-11-05 19:31:53 -0300

    calendario en vista de informe (luego lo cambio xd)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection  = require('express-myconnection');
 
+
+router.use(
+
+	connection(mysql, {
+		  host: '127.0.0.1',
+		  user: 'admin',
+		  password: 'tempo123',
+		  port: 3306,
+		  database: 'mydb'
+	}, 'pool')
+);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('main/index', { title: 'Bienvenido' });
@@ -17,7 +30,12 @@ router.get('/render_section/:id_frame', function(req, res, next){
 		        res.render('main/flujo_view');
 		        break;
 		    case 'informe':
-		        res.render('main/informe_view');
+		    	req.getConnection(function(err, connection){
+		    		connection.query("SELECT fecha_p,detalle FROM egreso WHERE fecha_p BETWEEN '2017-11-01 00:00:00' AND '2017-11-31 00:00:00'", function(err, rows){
+		    			if(err){console.log("Error Selecting : %s", err);}
+				        res.render('main/informe_view', {databd: rows});
+		    		});
+		    	});
 		        break;
 		    default:
 		        res.render('main/cc_view');
