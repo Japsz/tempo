2e8681e0d3e45a70483a94fec7199a4e9ae12f2d
Japsz <benjamin.meneses.14@sansano.usm.cl>
2017-12-16 23:24:45 -0300

    este cambió
    era un puto parentesis

--- db.sql
+++ db.sql
@@ -25,6 +25,7 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`egreso` (
   `idegreso` INT(11) NOT NULL AUTO_INCREMENT,
+  `idcdc` INT(11) NOT NULL,
   `idpago` INT(11) NOT NULL,
   `monto` INT(11) NULL DEFAULT NULL,
   `n_factura` VARCHAR(45) NULL DEFAULT NULL,
@@ -37,6 +38,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`egreso` (
     FOREIGN KEY (`idpago`)
     REFERENCES `mydb`.`pago` (`idpago`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  INDEX `fk_egreso_idcdc_idx` (`idcdc` ASC),
+  CONSTRAINT `fk_egreso_cdc`
+    FOREIGN KEY (`idcdc`)
+    REFERENCES `mydb`.`cdc` (`idcdc`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
     ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
@@ -47,13 +54,20 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`ingreso` (
   `idingreso` INT(11) NOT NULL AUTO_INCREMENT,
-  `idpago` INT(11) NOT NULL,
+  `idpago` INT(11) NULL,
+  `idcdc` INT(11) NULL,
   `monto` INT(11) NULL DEFAULT NULL,
   `n_factura` VARCHAR(45) NULL DEFAULT NULL,
   `fecha` DATETIME NULL DEFAULT NULL,
   `fecha_p` DATETIME NULL DEFAULT NULL,
   `detalle` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`idingreso`),
+    INDEX `fk_ingreso_idcdc_idx` (`idcdc` ASC),
+  CONSTRAINT `fk_ingreso_cdc`
+    FOREIGN KEY (`idcdc`)
+    REFERENCES `mydb`.`cdc` (`idcdc`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   INDEX `fk_ingreso_idpago_idx` (`idpago` ASC),
   CONSTRAINT `fk_ingreso_pago`
     FOREIGN KEY (`idpago`)
