f2d9efa01c968f505eaaee2158a5a696f0465272
daguila27 <daguila.gallardo@gmail.com>
2017-12-20 03:48:19 -0300

    seccion mi empresa->mis gastos

--- routes/cdc.js
+++ routes/cdc.js
@@ -37,52 +37,93 @@ router.post('/save',function(req,res,next){
         });
     });
 });
+
+
+function js_yyyy_mm_dd_hh_mm_ss () {
+  now = new Date();
+  year = "" + now.getFullYear();
+  month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+  day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
+  hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+  minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+  second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+  return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+}
+
 router.post('/save_pay',function(req,res,next){
     var input = JSON.parse(JSON.stringify(req.body));
     input.tipo = input.tipo.toLowerCase();
     var tipo = input.tipo;
     req.getConnection(function(err,connection){
         if(err) throw err;
-        if(input.fac == "1"){
-            delete input.fac;
-            input.fecha_p = input.fecha;
-            connection.query("INSERT INTO pago SET ?", input, function(err, pago){
+        if(input.tipo == 'egreso'){
+            console.log(input);
+            input.fechacreacion = js_yyyy_mm_dd_hh_mm_ss();
+            delete input.metodopago;
+            delete input.tipo;
+            connection.query("INSERT INTO "+tipo+" SET ?", input, function(err, rows){
                 if(err){console.log("Error Selecting : %s", err);}
-                delete input.tipo;
-                delete input.n_factura;
-                delete input.fecha_p;
-                input.idpago = pago.insertId;
-                connection.query("INSERT INTO " + tipo + " SET ?",input,function(err,rows){
-                    if(err) throw err;
-                    res.redirect('/cdc/show/' + input.idcdc);
-                });
+                res.redirect('/cdc/mycdc_gastos');
             });
         }
         else{
+            if(input.fac == "1"){
                 delete input.fac;
-                delete input.tipo;
-                delete input.n_factura;
-                input.idpago = null;
-                connection.query("INSERT INTO " + tipo + " SET ?",input,function(err,rows){
-                    if(err) throw err;
-                    res.redirect('/cdc/show/' + input.idcdc);
-                });  
+                input.fecha_p = input.fecha;
+                connection.query("INSERT INTO pago SET ?", input, function(err, pago){
+                    if(err){console.log("Error Selecting : %s", err);}
+                    delete input.tipo;
+                    delete input.n_factura;
+                    delete input.fecha_p;
+                    input.idpago = pago.insertId;
+                    connection.query("INSERT INTO " + tipo + " SET ?",input,function(err,rows){
+                        if(err) throw err;
+                        res.redirect('/cdc/show/' + input.idcdc);
+                    });
+                });
+            }
+            else{
+                    delete input.fac;
+                    delete input.tipo;
+                    delete input.n_factura;
+                    input.idpago = null;
+                    connection.query("INSERT INTO " + tipo + " SET ?",input,function(err,rows){
+                        if(err) throw err;
+                        res.redirect('/cdc/show/' + input.idcdc);
+                    });  
+            }
         }
-        
     });
 });
+
+
+
 router.get("/mycdc",function(req,res,next){
     req.getConnection(function(err,connection){
         if(err) throw err;
-        connection.query("SELECT cdc.*,SUM(egreso.monto) as e_total,SUM(ingreso.monto) as i_total FROM cdc LEFT JOIN egreso ON egreso.idcdc = cdc.idcdc WHERE cdc.idcdc = 1 GROUP BY cdc.idcdc",function(err,cdc){
+        connection.query("SELECT cdc.*,SUM(egreso.monto) as e_total FROM cdc LEFT JOIN egreso ON egreso.idcdc = cdc.idcdc WHERE cdc.idcdc = 1 GROUP BY cdc.idcdc",function(err,cdc){
             if(err) throw err;
             connection.query("SELECT * FROM egreso WHERE idcdc = 1",function(err,egresos){
                 if(err) throw err;
+                console.log(cdc);
+                console.log(egresos);
                 res.render('cdc/mycdc',{cdc: cdc[0],egreso:egresos});
             });
         });
     });
 });
+
+
+router.get("/mycdc_gastos",function(req,res,next){
+    req.getConnection(function(err,connection){
+        if(err) throw err;
+        connection.query("SELECT * FROM egreso WHERE idcdc = 1",function(err,egresos){
+                if(err) throw err;
+                res.render('cdc/mycdc_gastos',{egreso:egresos});
+            });
+    });
+});
+
 router.get("/show/:idcdc",function(req,res,next){
     req.getConnection(function(err,connection){
         if(err) throw err;
